b3be44488289ad4ba686c56fea076e38cd4d9221
Mateus Valentim <101218973+mattsu014@users.noreply.github.com>
2024-11-08 18:40:57 -0300

    att_port index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
 const pool = require('./db.js')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
